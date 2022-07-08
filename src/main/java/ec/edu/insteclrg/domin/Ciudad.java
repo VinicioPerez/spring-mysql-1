df15f1d6f21fb8eb2fe64b8fb7fd4eb648394478
Marco Perez <marcpvin@hotmail.com>
2022-07-07 21:28:10 -0500

    Provincia and Ciudad added

---
+++ src/main/java/ec/edu/insteclrg/domin/Ciudad.java
@@ -0,0 +1,31 @@
+package ec.edu.insteclrg.domin;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Ciudad {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long idCiudad;	
+	
+	@Column(nullable = false)
+	private String nombreCiudad;
+
+	@ManyToOne
+	@JoinColumn(name = "Provincia_idProvincia")
+	private Provincia provincia;
+
+}
